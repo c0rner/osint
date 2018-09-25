c34e9605ee9303a3cbdeb8d4a77584f2d46efc35
Martin Åkesson <2602099+c0rner@users.noreply.github.com>
2018-09-25 21:01:28 +0200

    Fixed label validation bug

--- domaindrain/drain.py
+++ domaindrain/drain.py
@@ -12,14 +12,17 @@ import wordfuzz
 # This regexp pattern is based on what is in the validators library
 # by Konsta Vesterinen (https://github.com/kvesteri/validators)
 label_re = re.compile(
-    r'^([a-zA-Z0-9][-_a-zA-Z0-9.]{0,61}[a-zA-Z0-9])+$'
+    r'^([a-zA-Z0-9][-_a-zA-Z0-9]{0,61})$'
     )
 tld_re = re.compile(
     r'^([a-zA-Z]{2,13}|(xn--[a-zA-Z0-9]{2,30}))$'  # TLD
     )
 
-def is_valid_label(label):
-    return label_re.match(label)
+def have_valid_labels(label):
+    for l in label.split('.'):
+        if not label_re.match(l.encode('idna')):
+            return False
+    return True
 
 def gen_help(ap, name, description, functions):
     group = parser.add_argument_group(name, description)
@@ -44,12 +47,9 @@ def set_default(obj):
 parser = argparse.ArgumentParser(description='Domain label fuzzer')
 parser.add_argument('label', type=str)
 gen_help(parser, 'Options', 'Fuzzing methods', wordfuzz.arg.methods)
-#gen_help(parser, 'Keyboard', 'Mutators based on keyboard layout', wordfuzz.keyboard.cmd.fuzzers)
-#gen_help(parser, 'Noise', 'Mutators based on noise', wordfuzz.noise.cmd.fuzzers)
-#gen_help(parser, 'Language', 'Mutators based on language constructs', wordfuzz.language.cmd.fuzzers)
 
 args = vars(parser.parse_args())
-if args['label'] is None:
+if args['label'] is None or args['functions'] is None:
     parser.print_help()
     exit()
 
@@ -59,13 +59,9 @@ result = {}
 for fuzzer in fuzzed_output:
     result[fuzzer] = {}
     for word in fuzzed_output[fuzzer]:
-        if not is_valid_label(word.encode("idna")):
+        if not have_valid_labels(word):
             continue
         result[fuzzer][word] = {}
         result[fuzzer][word]['idna'] = word.encode("idna")
         result[fuzzer][word]['python'] = word.encode("unicode_escape")
         print("{},{},{}".format(fuzzer, word.encode("idna"), word.encode("utf8")))
-
-#print("# Generated {} valid domain name variations ({} were discarded)".format(len(result),len(invalid_domains)))
-#print("JSON: {}".format(json.dumps(domains, indent=2, default=set_default, ensure_ascii=False).encode('utf8')))
-#print("JSON: {}".format(json.dumps(result, indent=2, ensure_ascii=False).encode('utf8')))
